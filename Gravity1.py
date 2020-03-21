5bf606738f65c87043ae957e30e1810958722bcb
Jelmer Mulder <jmulder14.jm@gmail.com>
2020-03-21 19:23:55 +0100

    Add force directions

--- Gravity1.py
+++ Gravity1.py
@@ -5,12 +5,13 @@ import random
 import math
 
 x, y = 0, 200
-G = 20000 #6.67428 * 10**(-11)
-dt = 0.001
-mass1 = 5
-mass2 = 5
-xlocation = [-0.3, 0.3]
-ylocation = [2, -2]
+G = 10000 #6.67428 * 10**(-11)
+dt = 0.01
+mass1 = 2
+mass2 = 2
+xlocation = [-2, 0]
+ylocation = [1, -1]
+time = 0
 
 def grav_force(x1, y1, x2, y2, mass1, mass2, G):
     angle =  math.atan((y2 - y1)/(x2 - x1))      #deltay/deltax
@@ -18,6 +19,18 @@ def grav_force(x1, y1, x2, y2, mass1, mass2, G):
     force = G * mass1 * mass2 / dr**2
     xforce = math.cos(angle) * force 
     yforce = math.sin(angle) * force
+    if x1 < x2 and y1>y2:
+        xforce = xforce
+        yforce = yforce
+    elif x1> x2 and y1> y2:
+        xforce = -1 * xforce
+        yforce = -1 * yforce
+    elif x1 < x2 and y1 < y2:
+        xforce = xforce
+        yforce = yforce
+    else:
+        xforce = -1 * xforce
+        yforce = -1* yforce
     return xforce, yforce
 
 def displacement(force, mass, dt):
@@ -26,21 +39,20 @@ def displacement(force, mass, dt):
     dx = 1/2 * a * dt**2
     return dx
 
-for i in range(2000):
+for i in range(200):
     xforce, yforce = grav_force(xlocation[0], ylocation[0], xlocation[1], ylocation[1], mass1, mass2, G)
     dx1, dy1 = displacement(xforce, mass1, dt), displacement(yforce, mass1, dt)
-    dx2, dy2 = 0, 0
-    print(dx1)
+    dx2, dy2 = 5* math.cos(time), 5* math.sin(time)
     xlocation[0] = xlocation[0]+ dx1
     ylocation[0] = ylocation[0]+ dy1
-    xlocation[1] = xlocation[1] + dx2
-    ylocation[1] = ylocation[1] + dy2
-
+    xlocation[1] = dx2
+    ylocation[1] =dy2
+    time += 10* dt
     plt.plot(xlocation[0], ylocation[0], 'ro', xlocation[1], ylocation[1], 'go')
-    plt.xlim(-1.5, 1.5)
-    plt.ylim(-5, 5)
+    plt.xlim(-10, 10)
+    plt.ylim(-10, 10)
     plt.draw()
-    plt.pause(0.000001)
+    plt.pause(0.00001)
     plt.clf()
 
 
