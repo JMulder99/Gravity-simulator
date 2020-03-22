f22b5e5c39d3f799ee63db1e2d3dc229f2b6d8b0
Jelmer Mulder <jmulder14.jm@gmail.com>
2020-03-22 12:17:54 +0100

    Add vzero and comments

--- Gravity1.py
+++ Gravity1.py
@@ -7,24 +7,28 @@ import numpy as np
 import random
 import math
 
-# Global variables
-x, y = 0, 200
-G = 20000 #6.67428 * 10**(-11)
-dt = 0.001
+# global variables
+# particles info
 mass1 = 20
 mass2 = 20
-xlocation = [0.9, -1]
-ylocation = [1, -1]
-time = 0
+xlocation = [-2, 2]
+ylocation = [1, -2]
+xspeed = [0, 10]
+yspeed = [-10, 15]
+
+# constants
+G = 200 #6.67428 * 10**(-11)
+dt = 0.001
 
 # Force function
 def grav_force(x1, y1, x2, y2, mass1, mass2, G):
     angle =  math.atan((y2 - y1)/(x2 - x1))      #deltay/deltax
-    dr = max([((x1-x2)**2 + (y1-y2)**2)**0.5, 1])
-    print(dr)
+    dr = max(((x1-x2)**2 + (y1-y2)**2)**0.5, 0.2)
     force = G * mass1 * mass2 / dr**2
     xforce = math.cos(angle) * force 
     yforce = math.sin(angle) * force
+
+    # The direction of force depends on relative location
     if x1 < x2 and y1>y2:
         xforce = xforce
         yforce = yforce
@@ -40,27 +44,35 @@ def grav_force(x1, y1, x2, y2, mass1, mass2, G):
     return xforce, yforce
 
 # function which calculate a tiny displacement during dt seconds
-def displacement(force, mass, dt):
-    # F = m a
-    a = force / mass
-    dx = 1/2 * a * dt**2
-    return dx
+def displacement(force, mass, v, dt):
+    a = force / mass # F = m a
+    v = v + a * dt
+    dx = v * dt # dx = v0*dt + a * t * dt
+    return dx, v
 
 # for loop which draws the dots on a figure
-for i in range(200):
-    xforce, yforce = grav_force(xlocation[0], ylocation[0], xlocation[1], ylocation[1], mass1, mass2, G)
-    dx1, dy1 = displacement(xforce, mass1, dt), displacement(yforce, mass1, dt)
-    dx2, dy2 = 5* math.cos(time), 5* math.sin(time)
+for i in range(600):
+    #particle 1
+    xforce1, yforce1 = grav_force(xlocation[0], ylocation[0], xlocation[1], ylocation[1], mass1, mass2, G)
+    dx1, xspeed[0] = displacement(xforce1, mass1, xspeed[0], dt)
+    dy1, yspeed[0] = displacement(yforce1, mass1, yspeed[0], dt)
+    # update location particle 1
     xlocation[0] += dx1
     ylocation[0] += dy1
-    xlocation[1] = dx2
-    ylocation[1] =dy2
-    time += 15* dt
+    # particle 2
+    xforce2, yforce2 = grav_force(xlocation[1], ylocation[1], xlocation[0], ylocation[0], mass1, mass2, G)
+    dx2, xspeed[1] = displacement(xforce2, mass2, xspeed[1], dt)
+    dy2, yspeed[1] = displacement(yforce2, mass2, yspeed[1], dt)
+    # update location particle 2
+    xlocation[1] += dx2
+    ylocation[1] += dy2
+
+    # plot the two dots using matplotlib
     plt.plot(xlocation[0], ylocation[0], 'ro', xlocation[1], ylocation[1], 'go')
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
     plt.draw()
-    plt.pause(0.00001)
+    plt.pause(0.000001)
     plt.clf()
 
 
